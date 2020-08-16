e5838a22760c3c70ddd47640979e7a9417612ad9
augustoabe <augusto.takeo.ismart@gmail.com>
2020-08-16 17:58:37 -0300

    Code for plot 1 with figures

--- plot1.R
+++ plot1.R
@@ -1,7 +1,8 @@
 file <- read.table("household_power_consumption.txt", 
                    sep = ";", header = TRUE)
-globalap <- file[,"Global_active_power"]
-globalap <- as.numeric(globalap)
-globalap <- globalap[!is.na(globalap)]
-hist(globalap, main = "Global Active Power", col = "red", 
+png(file = "plot1.png")
+f <- filter(file, Date == "1/2/2007"|Date == "2/2/2007")
+f[,3] <- as.numeric(f[,3])
+hist(f[,3], col = "red", main = "Global Active Power", 
      xlab = "Global Active Power")
+dev.off()
