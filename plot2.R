3c3bd202a9a4dd63ccd449f75368e2065566d028
augustoabe <augusto.takeo.ismart@gmail.com>
2020-08-18 23:51:56 -0300

    Plot1

--- plot2.R
+++ plot2.R
@@ -1,8 +1,8 @@
 file <- read.table("household_power_consumption.txt", 
                    sep = ";", header = TRUE)
-
 png(file = "plot2.png")
 f <- filter(file, Date == "1/2/2007"|Date == "2/2/2007")
 f[,3] <- as.numeric(f[,3])
-plot(1:nrow(f), f[,3], type = 'l')
+plot(1:nrow(f), f[,3], ylab = "Global Active Power (kilowatts)",
+     xlab = "", type = 'l')
 dev.off()
